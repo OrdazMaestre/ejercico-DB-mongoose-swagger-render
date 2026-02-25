b4fc8800b353ac11d1b4442fd4f594305a53f586
OrdazMaestre <ordazmaestre@gmail.com>
2026-02-25 17:50:41 +0100

    SWAGGER_RENDER_INTENTÁNDOLO

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-import "dotenv/config";
 
-// const PORT = 8080;
-const PORT = process.env.PORT || 3000;
+// Cargar variables de entorno (si usas .env)
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;  // Render usa process.env.PORT (normalmente 10000)
 
 const { dbConnection } = require('./config/config');
 const routes = require('./routes');
@@ -16,11 +17,11 @@ app.use(express.json());
 
 app.use('/', routes);
 
-// Endpoint Swagger
+// Endpoint de Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(docs));
 
 dbConnection();
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started on port ${PORT}`);
 });
